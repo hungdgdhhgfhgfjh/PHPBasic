d75b27f3b817787629a8ff65a8d84757ecedf75f
Hưng Randy <89236484+hungdgdhhgfhgfjh@users.noreply.github.com>
2021-12-30 12:42:07 +0700

    Update UserController.php

--- notePHP/Controller/UserController.php
+++ notePHP/Controller/UserController.php
@@ -1,5 +1,7 @@
 <?php
 namespace Controller;
+error_reporting(-1);
+ini_set('display_errors', 'On');
 include_once "./Models/User.php";
 use Models\User;
 
@@ -7,29 +9,15 @@ use Models\User;
 class UserController{
     public function register()
     {
-        
         include_once "./Views/user/register.php";
     }
     public function HandleRegister()
     {
-        
-        //    echo "<pre>";
-        //    print_r($_REQUEST);
-        //    echo "<pre>";
-        //    Array
-        // (
-        //     [users] => HandleRegister
-        //     [name] => hOÀNG ANH
-        //     [email] => hungrandy@gmail.com
-        //     [password] => hungpro123
-        //     [numberPhone] => 0921687290
-        // )
         $required_fields = [
             'name' => 'tên',
             'email' => 'email',
             'password' => 'mật khẩu',
             'numberPhone' => 'số điện thoại',
-            
         ];
         $_SESSION["errors"] = [];
         $errors = [];
@@ -63,17 +51,6 @@ class UserController{
     }
     public function handleLogin()
     {
-         //    echo "<pre>";
-        //    print_r($_REQUEST);
-        //    echo "<pre>";
-        //    Array
-        // (
-        //     [users] => HandleRegister
-        //     [name] => hOÀNG ANH
-        //     [email] => hungrandy@gmail.com
-        //     [password] => hungpro123
-        //     [numberPhone] => 0921687290
-        // )
         $required_fields = [
             'email' => 'email',
             'password' => 'mật khẩu',
@@ -106,5 +83,75 @@ class UserController{
             }
         }
         
+    }
+    public function show()
+    {
+        $id = (isset($_REQUEST["id"])  && !empty($_REQUEST["id"]))?$_REQUEST["id"]:"";
+        if(empty($id)){
+            header("location:index.php?home=home");
+        }
+        $userModel = new User();
+
+        $user = $userModel->show($id);
+        include_once "./Views/user/show.php";
+    }
+    public function logout()
+    {
+        unset($_SESSION['user']);
+        header("location:index.php?home=home");
+
+    }
+    public function edit()
+    {
+        $id = (isset($_REQUEST["id"])  && !empty($_REQUEST["id"]))?$_REQUEST["id"]:"";
+        if(empty($id)){
+            header("location:index.php?home=home");
+        }
+        $userModel = new User();
+
+        $user = $userModel->show($id);
+        include_once "./Views/user/edit.php";
+        
+    }
+    public function update()
+    {
+        $id = (isset($_REQUEST["id"])  && !empty($_REQUEST["id"]))?$_REQUEST["id"]:"";
+        if(empty($id)){
+            header("location:index.php?home=home");
+        }
+        $required_fields = [
+            'name' => 'tên',
+            'email' => 'email',
+            'password' => 'mật khẩu',
+            'numberPhone' => 'số điện thoại',
+        ];
+        $_SESSION["errors"] = [];
+        $errors = [];
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+        {
+            $name        = $_REQUEST['name'];
+            $email       = $_REQUEST['email'];
+            $password    = $_REQUEST['password'];
+            $numberPhone = $_REQUEST['numberPhone'];
+
+            foreach ($required_fields as $field => $label) {
+                if ($_POST[$field] == '') {
+                    $errors[$field] = "Vui lòng nhập " . $label;
+                }
+             }
+            if(count($errors) === 0){
+                    $data = $_REQUEST;
+                    $userModel = new User();
+                    $userModel->update($data,$id);
+                    $user = $userModel->show($id);
+                    $_SESSION["user"] = $user;
+                    header("location: index.php?home=home");
+
+            }
+            else{
+                $_SESSION["errors"] = $errors;
+                header("location:index.php?users=edit&&id=".$id);
+            }
+        }
     }
 }
